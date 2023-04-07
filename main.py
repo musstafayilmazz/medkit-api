4702980154b406abb318de23a6d1e554619dd294
musstafayilmazz <musstafayilmaz96@gmail.com>
2023-04-07 19:02:04 +0300

    time function added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from eMail import email_send
+from datetime import datetime, timedelta, timezone
 
 app = FastAPI()
 
@@ -29,3 +30,9 @@ async def send_email(email_request: EmailRequest):
         return {"message": "E-mail sent!"}
     except:
         return {"message": "Unauth access!"}
+
+@app.get("/get_time")
+def get_turkey_time():
+    tz = timezone(timedelta(hours=3))  # Türkiye saat dilimi
+    now = datetime.now(tz)
+    return {"turkey_time": now.strftime("%Y-%m-%d %H:%M:%S")}
